f3ec3fab7fb8ddbbe0d5a711586de0bcc7824921
Matt Macdonald <mksmacdonald@gmail.com>
2021-10-10 09:39:16 -0700

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println("POMODORO TIME BABY!!!!");
+  delay(2500);
+  // put your main code here, to run repeatedly:
+}
